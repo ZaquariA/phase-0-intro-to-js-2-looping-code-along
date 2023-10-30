77fb5c3a7d12dedf2c1515c49d6388580a701663
zaquari_andl <zaquariandl043@gmail.com>
2023-10-30 17:54:26 -0500

    code along lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = []
+    for (let i=0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+function countDown() {
+    let count = 10;
+    while (count > -1) {
+        console.log(count--);
+    }
+}
